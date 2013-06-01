7506b6c47ae57189661e11f30c5b82644e82a65b
Sebastian Nowicki <sebnow@gmail.com>
2013-06-01 15:28:14 +0800

    Add Build.PL and main module

---
+++ lib/Messaging/BalancingDispatcher.pm
@@ -0,0 +1,31 @@
+package Messaging::BalancingDispatcher;
+use strict;
+use warnings;
+
+our $VERSION = v0.0.1;
+
+1;
+
+=head1 NAME
+
+Messaging::BalancingDispatcher - Master-Worker messaging pattern
+
+=head1 DESCRIPTION
+
+An implementation of the Master-Worker messaging pattern for
+distributing work, only to nodes that can work on it immediately.
+
+This pattern enables increasing the capacity of the system by the
+addition of L<Messaging::BalancingDispatcher::Worker|Workers>
+dynamically.
+
+=head1 AUTHORS
+
+=over
+
+=item Sebastian Nowicki <sebnow@gmail.com>
+
+=back
+
+=cut
+
